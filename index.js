b75a96e7839bdb53d13323701b65e71abcb64421
Swaibu Rumbiiha <rumbiihas@gmail.com>
2019-11-20 22:24:57 +0200

    setup basic domain

---
+++ index.js
@@ -0,0 +1,32 @@
+var subdomain 	= require('express-subdomain');
+var express 	  = require('express');
+var app 		     = express();
+
+
+var router = express.Router();
+ 
+app.use(subdomain('api', router));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+//api specific routes 
+router.get('/', function(req, res) {
+    res.send('Welcome to our API!');
+});
+ 
+router.get('/users', function(req, res) {
+    res.json([
+        { name: "Brian" }
+    ]);
+});
+
+var server = app.listen(process.env.PORT || 3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
